f9a88fe025a5fe16250b8e1b2cc4b19b19a490e6
Manuel Fernandez <manferlo81@gmail.com>
2019-07-21 18:19:36 +0700

    static folder use example

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,28 +1,39 @@
+import { readFile } from "fs";
+import { join } from "path";
 import React, { Component } from "react";
+import { promisify } from "util";
+
+const read = promisify(readFile);
+
+declare const __static: string;
 
 interface AppState {
   name: string;
   description: string;
+  text: string;
 }
 
 class App extends Component<{}, AppState> {
 
-  public state = {
+  public state: AppState = {
     name: "",
     description: "",
+    text: "",
   };
 
   public async componentDidMount() {
     const { name, description } = await import("../../package.json");
-    this.setState({ name, description });
+    const buffer = await read(join(__static, "text.txt"));
+    this.setState({ name, description, text: buffer.toString() });
   }
 
   public render() {
-    const { name, description } = this.state;
+    const { name, description, text } = this.state;
     return (
       <div>
         <h1>{name}</h1>
         <p>{description}</p>
+        <p>{text}</p>
       </div>
     );
   }
